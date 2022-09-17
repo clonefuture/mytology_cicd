379f2551d9a2d0539916fe81146c3b862b131b54
clonefuture <clonefuture@gmail.com>
2022-09-17 20:21:15 +1000

    to heroku

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import rest_framework.pagination
@@ -143,3 +144,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1
 }
+
+django_heroku.settings(locals())
